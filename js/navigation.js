b95d90f490c33b4b70a99695d046be31f147229a
josephmalete1997 <josephmalete75@gmail.com>
2023-09-24 18:51:55 +0000

    mobile navigation

--- js/navigation.js
+++ js/navigation.js
@@ -3,14 +3,15 @@ nav.style.transition = "1s";
 nav.style.left = "-100%";
 nav.style.display = "flex";
 document.querySelector(".fa-bars").addEventListener('click', () => {
-
-    nav.style.background = "white";
     nav.style.width = "100%";
     nav.style.height = "100%";
     nav.style.margin = "0px";
     nav.style.zIndex = "150";
     nav.style.top = "0";
-    nav.style.position = "absolute";
     nav.style.left = "0%";
     nav.style.transition = "1s";
+    setTimeout(() => {
+        nav.style.backgroundSize = "1000%";
+        nav.style.transition = "5s";
+    }, 2000);
 });
