f2dd6799c2f87a9b09e485e5fb05809917b62a93
josephmalete1997 <josephmalete75@gmail.com>
2023-09-27 17:56:45 +0000

    commit new

--- js/printing.js
+++ js/printing.js
@@ -7,6 +7,8 @@ const images = [
     ["printing/HD1.jpg", "printing/HD2.jpg", "printing/HD3.jpg", "printing/HD4.jpg"],
     ["printing/SU1.jpg", "printing/SU1B.jpg", "printing/SU2.jpg", "printing/SU2B.jpg", "printing/SU3.jpg", "printing/SU3B.jpg", "printing/SU4.jpg", "printing/SU4B.jpg"],
     ["printing/JS1.jpg", "printing/JS2.jpg", "printing/JS3.jpg", "printing/JS4.jpg"],
+    ["Branding/mugs/M1.jpg", "Branding/mugs/M2.jpg", "Branding/mugs/M3.jpg", "Branding/mugs/M4.jpg", "Branding/mugs/M5.jpg", "Branding/mugs/M6.jpg"],
+    ["phones/phone/p1.webp", "phones/phone/p2.webp", "phones/phone/p3.webp", "phones/phone/p4.jpg"]
 ]
 
 
@@ -27,6 +29,19 @@ slides[2].style.backgroundSize = "70%";
 slides[2].style.backgroundRepeat = "no-repeat";
 slides[2].style.backgroundPosition = "center";
 slides[2].style.transition = `0.2s`;
+
+slides[3].style.backgroundImage = "url(Branding/mugs/M1.jpg)";
+slides[3].style.backgroundSize = "70%";
+slides[3].style.backgroundRepeat = "no-repeat";
+slides[3].style.backgroundPosition = "center";
+slides[3].style.transition = `0.5s`;
+
+slides[4].style.backgroundImage = "url(phones/phone/p1.webp)";
+slides[4].style.backgroundSize = "70%";
+slides[4].style.backgroundRepeat = "no-repeat";
+slides[4].style.backgroundPosition = "center";
+slides[4].style.transition = `0.5s`;
+
 
 countOne = 0;
 leftKey[0].addEventListener('click', () => {
@@ -95,7 +110,50 @@ rightKey[2].addEventListener('click', () => {
 })
 
 
+countFour = 0;
+leftKey[3].addEventListener('click', () => {
+    countFour--;
+    slides[3].style.backgroundImage = `url(${images[3][countFour]})`;
+    slides[3].style.transition = `0.2s`;
+    if (countFour < 0) {
+        countFour = images[3].length - 1;
+        slides[3].style.backgroundImage = `url(${images[3][countFour]})`;
+        slides[3].style.transition = `0.2s`;
+    }
+});
+rightKey[3].addEventListener('click', () => {
+    countFour++;
+    slides[3].style.backgroundImage = `url(${images[3][countFour]})`;
+    slides[3].style.transition = `0.2s`;
+    if (countFour > images[3].length - 1) {
+        countFour = 0;
+        slides[3].style.backgroundImage = `url(${images[3][countFour]})`;
+        slides[3].style.transition = `0.2s`;
+    }
+})
 
+
+countFive = 0;
+leftKey[4].addEventListener('click', () => {
+    countFive--;
+    slides[4].style.backgroundImage = `url(${images[4][countFive]})`;
+    slides[4].style.transition = `0.2s`;
+    if (countFive < 0) {
+        countFive = images[3].length - 1;
+        slides[4].style.backgroundImage = `url(${images[4][countFive]})`;
+        slides[4].style.transition = `0.2s`;
+    }
+});
+rightKey[4].addEventListener('click', () => {
+    countFive++;
+    slides[4].style.backgroundImage = `url(${images[4][countFive]})`;
+    slides[4].style.transition = `0.2s`;
+    if (countFive > images[4].length - 1) {
+        countFive = 0;
+        slides[4].style.backgroundImage = `url(${images[4][countFive]})`;
+        slides[4].style.transition = `0.2s`;
+    }
+})
 
 
 
@@ -105,41 +163,22 @@ let pictures = ["printing/HD1.jpg", "printing/HD2.jpg", "printing/HD3.jpg", "pri
 
 let horizontal_items = document.querySelector("#horizontal-scroll");
 
-horizontal_items.style.background = "orange";
+horizontal_items.style.background = "white";
 horizontal_items.style.zIndex = "180";
 
 
 
 for (let i = 0; i < pictures.length; i++) {
     let homeItems = document.createElement("div");
-    homeItems.style.width = "500px";
-    homeItems.style.height = "400px";
+    homeItems.style.width = "600px";
+    homeItems.style.height = "500px";
     // homeItems.style.float = "left";
     homeItems.style.backgroundImage = `url(${pictures[i]})`;
     homeItems.style.backgroundPosition = "center";
     homeItems.style.backgroundRepeat = "no-repeat";
-    homeItems.style.margin = "-80px";
-    homeItems.style.backgroundSize = "60%";
+    homeItems.style.margin = "-150px";
+    homeItems.style.backgroundSize = "40%";
 
     horizontal_items.appendChild(homeItems);
 }
-
-
-//Scrolling
-
-
 // script.js
-
-counter = 0;
-
-function move() {
-    if (counter <= 0) {
-        counter -= 2;
-        document.querySelector('#horizontal-scroll').style.transform = `translateX(${counter}%)`;
-        document.querySelector('#horizontal-scroll').style.transition = "1s";
-        document.querySelector('.test').innerHTML = ` Counter = ${counter}`;
-    } else if (counter == -20) {
-        counter -= 0;
-    }
-}
-setInterval(move, 1000);
